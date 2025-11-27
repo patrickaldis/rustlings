df651d78b4bff2d8a4687b9dbe81f857dca02c33
Patrick Aldis <patricktaldis@gmail.com>
2025-11-27 10:24:33 +0000

    update shell

--- shell.nix
+++ shell.nix
@@ -1,16 +1,23 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rustVersion = "latest";
+  #rustVersion = "1.62.0";
+  rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+    extensions = [
+      "rust-src" # for rust-analyzer
+      "rust-analyzer"
+    ];
+  };
+in
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    rustlings
-    rust-analyzer-unwrapped
-    rustup
-    rustPlatform.bindgenHook
-  ];
-  RUSTC_VERSION = "stable";
-  # https://github.com/rust-lang/rust-bindgen#environment-variables
-  shellHook = ''
-    export PATH="''${CARGO_HOME:-~/.cargo}/bin":"$PATH"
-  '';
+  buildInputs = [
+    rust
+  ] ++ (with pkgs; [
+    pkg-config
+    # other dependencies
+    #gtk3
+    #wrapGAppsHook
+  ]);
+  RUST_BACKTRACE = 1;
 }
