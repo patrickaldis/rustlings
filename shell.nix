38419735f015ac7aef1a8025b8eaaf03192cdc0e
Patrick Aldis <patrick.aldis@virginmedia.com>
2025-11-27 08:31:31 +0000

    fixup! init

--- shell.nix
+++ shell.nix
@@ -5,5 +5,12 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     rustlings
     rust-analyzer-unwrapped
+    rustup
+    rustPlatform.bindgenHook
   ];
+  RUSTC_VERSION = "stable";
+  # https://github.com/rust-lang/rust-bindgen#environment-variables
+  shellHook = ''
+    export PATH="''${CARGO_HOME:-~/.cargo}/bin":"$PATH"
+  '';
 }
